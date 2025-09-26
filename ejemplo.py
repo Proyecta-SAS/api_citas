de0338e40ea749e8807fbf8b9ab546ca1b8042e0
TecnologiaAvanzar <tecnologia.avanzar@avanzarsoluciones.com>
2025-09-26 16:34:50 -0500

    v9.10
    Se ampliaron y actualizaron los datos del calendario simulado en load_calendar con nuevas entradas de eventos más detalladas. Se ajustó la carga útil en la interfaz principal para usar intervalos de 10 minutos y se eliminó el campo redundante "citas", lo que agilizó el envío de datos al backend.

--- ejemplo.py
+++ ejemplo.py
@@ -45,35 +45,122 @@ def load_calendar(argv):
 
     # Muestra mínima con el mismo shape, igual a lo solicitado
     return [
-        {
-            "body": {
+    {
+        "body": {
             "result": [
                 {
-                    "ID": "914272",
-                    "PARENT_ID": "914266",
+                    "ID": "1026440",
+                    "PARENT_ID": "1026440",
+                    "ACTIVE": "Y",
+                    "DELETED": "N",
+                    "CAL_TYPE": "user",
+                    "OWNER_ID": "17890",
+                    "NAME": "llamada a Juan Aldana (Prueba)",
+                    "DATE_FROM": "27/09/2025 09:20:00",
+                    "DATE_TO": "27/09/2025 09:30:00",
+                    "ORIGINAL_DATE_FROM": "null",
+                    "TZ_FROM": "America/Atikokan",
+                    "TZ_TO": "America/Atikokan",
+                    "TZ_OFFSET_FROM": "-18000",
+                    "TZ_OFFSET_TO": "-18000",
+                    "DATE_FROM_TS_UTC": "1758972000",
+                    "DATE_TO_TS_UTC": "1758972600",
+                    "DT_SKIP_TIME": "N",
+                    "DT_LENGTH": 600,
+                    "EVENT_TYPE": "null",
+                    "CREATED_BY": "16448",
+                    "DATE_CREATE": "25/09/2025 00:55:25",
+                    "TIMESTAMP_X": "25/09/2025 00:55:25",
+                    "DESCRIPTION": "El cliente Juan Aldana (Prueba) agendó esta llamada para obtener la información necesaria sobre nuestro proceso.",
+                    "DT_FROM": "null",
+                    "DT_TO": "null",
+                    "PRIVATE_EVENT": "",
+                    "ACCESSIBILITY": "busy",
+                    "IMPORTANCE": "normal",
+                    "IS_MEETING": False,
+                    "MEETING_STATUS": "H",
+                    "MEETING_HOST": "17890",
+                    "MEETING": {
+                        "NOTIFY": False,
+                        "MEETING_CREATOR": 17890,
+                        "REINVITE": False,
+                        "ALLOW_INVITE": False,
+                        "HIDE_GUESTS": False,
+                        "HOST_NAME": "Alba Katterine Rincon Rodriguez",
+                        "LANGUAGE_ID": "la",
+                        "MAIL_FROM": "servicioalcliente1@impulsepbs.com"
+                    },
+                    "LOCATION": "",
+                    "REMIND": [],
+                    "COLOR": "",
+                    "TEXT_COLOR": "",
+                    "RRULE": "",
+                    "EXDATE": "",
+                    "DAV_XML_ID": "20250927T142000Z-ee8c7859dba8d05fd40369e9a7f3c0cf@avanzarsoluciones.bitrix24.es",
+                    "G_EVENT_ID": "",
+                    "DAV_EXCH_LABEL": "",
+                    "CAL_DAV_LABEL": "",
+                    "VERSION": "1",
+                    "ATTENDEES_CODES": [
+                        "U17890"
+                    ],
+                    "RECURRENCE_ID": "null",
+                    "RELATIONS": "",
+                    "SECTION_ID": "6884",
+                    "SYNC_STATUS": "null",
+                    "UF_CRM_CAL_EVENT": [
+                        "D_840950"
+                    ],
+                    "UF_WEBDAV_CAL_EVENT": False,
+                    "SECTION_DAV_XML_ID": "null",
+                    "DATE_FROM_FORMATTED": "Sat Sep 27 2025 09:20:00",
+                    "DATE_TO_FORMATTED": "Sat Sep 27 2025 09:30:00",
+                    "IS_DAYLIGHT_SAVING_TZ": "N",
+                    "SECT_ID": "6884",
+                    "OPTIONS": "null",
+                    "ATTENDEE_LIST": [
+                        {
+                            "id": 17890,
+                            "entryId": "1026440",
+                            "status": "H"
+                        }
+                    ],
+                    "COLLAB_ID": "null",
+                    "attendeesEntityList": [
+                        {
+                            "entityId": "user",
+                            "id": 17890
+                        }
+                    ],
+                    "~USER_OFFSET_FROM": 0,
+                    "~USER_OFFSET_TO": 0
+                },
+                {
+                    "ID": "1028396",
+                    "PARENT_ID": "1028394",
                     "ACTIVE": "Y",
                     "DELETED": "N",
                     "CAL_TYPE": "user",
                     "OWNER_ID": "17890",
                     "NAME": "Excelencia Katterine Rincón",
-                    "DATE_FROM": "22/09/2025 09:00:00",
-                    "DATE_TO": "22/09/2025 09:15:00",
-                    "ORIGINAL_DATE_FROM": None,
+                    "DATE_FROM": "29/09/2025 08:20:00",
+                    "DATE_TO": "29/09/2025 08:35:00",
+                    "ORIGINAL_DATE_FROM": "29/09/2025 09:00:00",
                     "TZ_FROM": "America/Bogota",
                     "TZ_TO": "America/Bogota",
                     "TZ_OFFSET_FROM": "-18000",
                     "TZ_OFFSET_TO": "-18000",
-                    "DATE_FROM_TS_UTC": "1747652400",
-                    "DATE_TO_TS_UTC": "1766385000",
+                    "DATE_FROM_TS_UTC": "1759141200",
+                    "DATE_TO_TS_UTC": "1759142100",
                     "DT_SKIP_TIME": "N",
                     "DT_LENGTH": 900,
-                    "EVENT_TYPE": None,
+                    "EVENT_TYPE": "null",
                     "CREATED_BY": "17890",
                     "DATE_CREATE": "20/05/2025 21:51:00",
-                    "TIMESTAMP_X": "16/09/2025 04:48:24",
+                    "TIMESTAMP_X": "26/09/2025 19:47:52",
                     "DESCRIPTION": "",
-                    "DT_FROM": None,
-                    "DT_TO": None,
+                    "DT_FROM": "null",
+                    "DT_TO": "null",
                     "PRIVATE_EVENT": "",
                     "ACCESSIBILITY": "busy",
                     "IMPORTANCE": "normal",
@@ -104,53 +191,44 @@ def load_calendar(argv):
                     ],
                     "COLOR": "",
                     "TEXT_COLOR": "",
-                    "RRULE": {
-                        "FREQ": "WEEKLY",
-                        "COUNT": 32,
-                        "INTERVAL": 1,
-                        "BYDAY": {
-                            "MO": "MO"
-                        },
-                        "UNTIL": "01/01/2038",
-                        "~UNTIL": "",
-                        "UNTIL_TS": 2145916800
-                    },
-                    "EXDATE": "15/09/2025;09/06/2025;23/06/2025;07/07/2025",
+                    "RRULE": "",
+                    "EXDATE": "",
                     "DAV_XML_ID": "20250519T140000Z-82a9304c457f248914dff05e6e974c94@avanzarsoluciones.bitrix24.es",
                     "G_EVENT_ID": "",
                     "DAV_EXCH_LABEL": "",
                     "CAL_DAV_LABEL": "",
-                    "VERSION": "20",
+                    "VERSION": "23",
                     "ATTENDEES_CODES": [
                         "U1127",
                         "U17890"
                     ],
-                    "RECURRENCE_ID": None,
-                    "RELATIONS": "",
+                    "RECURRENCE_ID": 914266,
+                    "RELATIONS": {
+                        "COMMENT_XML_ID": "EVENT_914266_29/09/2025"
+                    },
                     "SECTION_ID": "6884",
-                    "SYNC_STATUS": None,
+                    "SYNC_STATUS": "null",
                     "UF_CRM_CAL_EVENT": False,
                     "UF_WEBDAV_CAL_EVENT": False,
-                    "SECTION_DAV_XML_ID": None,
-                    "DATE_FROM_FORMATTED": "Mon Sep 22 2025 09:00:00",
-                    "DATE_TO_FORMATTED": "Mon Sep 22 2025 09:15:00",
+                    "SECTION_DAV_XML_ID": "null",
+                    "DATE_FROM_FORMATTED": "Mon Sep 29 2025 08:20:00",
+                    "DATE_TO_FORMATTED": "Mon Sep 29 2025 08:35:00",
                     "IS_DAYLIGHT_SAVING_TZ": "N",
                     "SECT_ID": "6884",
-                    "OPTIONS": None,
+                    "OPTIONS": "null",
                     "ATTENDEE_LIST": [
                         {
                             "id": 1127,
-                            "entryId": "914266",
+                            "entryId": "1028394",
                             "status": "H"
                         },
                         {
                             "id": 17890,
-                            "entryId": "914272",
+                            "entryId": "1028396",
                             "status": "Y"
                         }
                     ],
-                    "COLLAB_ID": None,
-                    "~RRULE_DESCRIPTION": "semanalmente: Lun, desde 19/05/2025, 32 tiempo(s)",
+                    "COLLAB_ID": "null",
                     "attendeesEntityList": [
                         {
                             "entityId": "user",
@@ -161,27 +239,135 @@ def load_calendar(argv):
                             "id": 17890
                         }
                     ],
-                    "~DATE_FROM": "19/05/2025 09:00:00",
-                    "~DATE_TO": "19/05/2025 09:15:00",
-                    "RINDEX": 1,
+                    "~USER_OFFSET_FROM": 0,
+                    "~USER_OFFSET_TO": 0
+                },
+                {
+                    "ID": "1022964",
+                    "PARENT_ID": "1022960",
+                    "ACTIVE": "Y",
+                    "DELETED": "N",
+                    "CAL_TYPE": "user",
+                    "OWNER_ID": "17890",
+                    "NAME": "LECCIONES APRENDIDAS AL JUAN PABLO DIAZ",
+                    "DATE_FROM": "02/10/2025 15:00:00",
+                    "DATE_TO": "02/10/2025 16:00:00",
+                    "ORIGINAL_DATE_FROM": "null",
+                    "TZ_FROM": "America/Bogota",
+                    "TZ_TO": "America/Bogota",
+                    "TZ_OFFSET_FROM": "-18000",
+                    "TZ_OFFSET_TO": "-18000",
+                    "DATE_FROM_TS_UTC": "1759424400",
+                    "DATE_TO_TS_UTC": "1759428000",
+                    "DT_SKIP_TIME": "N",
+                    "DT_LENGTH": 3600,
+                    "EVENT_TYPE": "null",
+                    "CREATED_BY": "17890",
+                    "DATE_CREATE": "22/09/2025 20:44:20",
+                    "TIMESTAMP_X": "22/09/2025 20:44:20",
+                    "DESCRIPTION": "",
+                    "DT_FROM": "null",
+                    "DT_TO": "null",
+                    "PRIVATE_EVENT": "",
+                    "ACCESSIBILITY": "busy",
+                    "IMPORTANCE": "normal",
+                    "IS_MEETING": True,
+                    "MEETING_STATUS": "Y",
+                    "MEETING_HOST": "16004",
+                    "MEETING": {
+                        "NOTIFY": True,
+                        "MEETING_CREATOR": 16004,
+                        "REINVITE": False,
+                        "ALLOW_INVITE": False,
+                        "HIDE_GUESTS": True,
+                        "HOST_NAME": "Ingrid Paola Julio Lascarro",
+                        "LANGUAGE_ID": "null",
+                        "MAIL_FROM": False,
+                        "CHAT_ID": 0
+                    },
+                    "LOCATION": "",
+                    "REMIND": [
+                        {
+                            "type": "min",
+                            "count": 15
+                        }
+                    ],
+                    "COLOR": "#00afc7",
+                    "TEXT_COLOR": "",
+                    "RRULE": "",
+                    "EXDATE": "",
+                    "DAV_XML_ID": "20251002T200000Z-3e0685ecfef9674e60cc0c84b5355631@avanzarsoluciones.bitrix24.es",
+                    "G_EVENT_ID": "",
+                    "DAV_EXCH_LABEL": "",
+                    "CAL_DAV_LABEL": "",
+                    "VERSION": "1",
+                    "ATTENDEES_CODES": [
+                        "U16004",
+                        "U1127",
+                        "U17890"
+                    ],
+                    "RECURRENCE_ID": "null",
+                    "RELATIONS": "",
+                    "SECTION_ID": "6884",
+                    "SYNC_STATUS": "null",
+                    "UF_CRM_CAL_EVENT": False,
+                    "UF_WEBDAV_CAL_EVENT": False,
+                    "SECTION_DAV_XML_ID": "null",
+                    "DATE_FROM_FORMATTED": "Thu Oct 02 2025 15:00:00",
+                    "DATE_TO_FORMATTED": "Thu Oct 02 2025 16:00:00",
+                    "IS_DAYLIGHT_SAVING_TZ": "N",
+                    "SECT_ID": "6884",
+                    "OPTIONS": "null",
+                    "ATTENDEE_LIST": [
+                        {
+                            "id": 16004,
+                            "entryId": "1022960",
+                            "status": "H"
+                        },
+                        {
+                            "id": 1127,
+                            "entryId": "1022962",
+                            "status": "Y"
+                        },
+                        {
+                            "id": 17890,
+                            "entryId": "1022964",
+                            "status": "Y"
+                        }
+                    ],
+                    "COLLAB_ID": "null",
+                    "attendeesEntityList": [
+                        {
+                            "entityId": "user",
+                            "id": 16004
+                        },
+                        {
+                            "entityId": "user",
+                            "id": 1127
+                        },
+                        {
+                            "entityId": "user",
+                            "id": 17890
+                        }
+                    ],
                     "~USER_OFFSET_FROM": 0,
                     "~USER_OFFSET_TO": 0
                 }
             ],
             "time": {
-                "start": 1758314944.790063,
-                "finish": 1758314944.834991,
-                "duration": 0.04492807388305664,
-                "processing": 0.019826173782348633,
-                "date_start": "2025-09-19T23:49:04+03:00",
-                "date_finish": "2025-09-19T23:49:04+03:00",
-                "operating_reset_at": 1758315544,
+                "start": 1758920424,
+                "finish": 1758920424.193102,
+                "duration": 0.1931018829345703,
+                "processing": 0,
+                "date_start": "2025-09-27T00:00:24+03:00",
+                "date_finish": "2025-09-27T00:00:24+03:00",
+                "operating_reset_at": 1758921024,
                 "operating": 0
             }
         },
         "headers": {
             "server": "nginx",
-            "date": "Fri, 19 Sep 2025 20:49:04 GMT",
+            "date": "Fri, 26 Sep 2025 21:00:24 GMT",
             "content-type": "application/json; charset=utf-8",
             "transfer-encoding": "chunked",
             "connection": "close",
@@ -199,19 +385,18 @@ def load_calendar(argv):
             "access-control-allow-headers": "origin, content-type, accept",
             "x-content-type-options": "nosniff, nosniff",
             "x-bitrix-rest-application": "local.685d5063933614.00854686",
-            "x-bitrix-rest-time": "0.0198261738",
-            "x-bitrix-rest-user-time": "0.0092240000",
-            "x-bitrix-rest-system-time": "0.0048570000",
+            "x-bitrix-rest-time": "0.0000000000",
+            "x-bitrix-rest-user-time": "0.0126380000",
+            "x-bitrix-rest-system-time": "0.0041390000",
             "x-frame-options": "SAMEORIGIN",
             "strict-transport-security": "max-age=31536000; includeSubdomains",
-            "server-timing": "t1;dur=0.180, t2;dur=0.179, t3;dur=0.000, tc1;dur=117500, tc2;dur=18750, tc3;dur=20",
-            "x-bitrix-ri": "b6be4e95e4acf11fea4078e3d6598581",
-            "x-bitrix-lb": "lb-sa"
+            "server-timing": "t1;dur=0.264, t2;dur=0.264, t3;dur=0.000, tc1;dur=112000, tc2;dur=17723, tc3;dur=20",
+            "x-bitrix-ri": "ecd91e03dedd97b763a0f87ce6b51af7",
+            "x-bitrix-lb": "lb-sa-2"
         },
         "statusCode": 200
-        }
-    ]
-
+    }
+]
 
 def calendar_to_citas(calendar):
     """Extrae citas.result con DATE_FROM y DATE_TO desde calendar[0].body.result"""
@@ -235,7 +420,7 @@ def main(argv):
 
     # Construir payload con filtros y el calendario en el formato requerido + citas derivadas
     payload = {
-        "minutos": 30,
+        "minutos": 10,
         "Cantidad_dias": 3,
         "filtro": {
             "horario" : {
@@ -246,8 +431,7 @@ def main(argv):
         },
         # Se envía el calendario crudo como lo recibiste
         "calendar": calendar,
-        # Y además, para que el backend bloquee solapes, se transforma a citas.result
-        "citas": calendar_to_citas(calendar)
+
     }
 
     data = json.dumps(payload, ensure_ascii=False).encode("utf-8")
